5bb5bda3b4b00ec19b6c0072ccd578010a8341db
Pablo Tapia <ptapia@proyecto404.com>
2019-08-05 15:45:54 -0300

    add notes

---
+++ app/components/Notes.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default ({ notes }) => (<ul>{notes.map(note =>
+    <li key={note.id}>{note.task}</li>)}</ul>
+)
--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-if(process.env.NODE_ENV !== 'production') {
+import App from './components/App';
+if (process.env.NODE_ENV !== 'production') {
   React.Perf = require('react-addons-perf');
 }
 
 ReactDOM.render(
-  <div>Hello world</div>,
+  <App />,
   document.getElementById('app')
 );
